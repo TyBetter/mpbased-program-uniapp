2bd8c7fd9ac86e5619fbb3fc5911e4a56623e96a
TyBetter <TyBetter@users.noreply.github.com>
2021-03-04 16:06:31 +0800

    新增blank组件

---
+++ src/components/blank.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <p>暂时没有{{text}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "blank",
+    props: {
+        text: {
+            type: String,
+            value: ""
+        }
+    },
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style lang="less" scoped>
+    .container {
+        display: flex;
+        width: 100%;
+        height: 80rpx;
+        padding: 15rpx 0 15rpx 0;
+        justify-content: center;
+        align-items: center;
+        margin-top: 30rpx;
+        border-top: 0.5px solid rgba(129, 129, 129, 0.822);
+
+        p {
+            font-size: 25rpx;
+            color: #ddd;
+            font-weight: 200;
+            letter-spacing: 1px;
+        }
+    }
+</style>
