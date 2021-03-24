2e1c6ffe9c30092640ad183cd9d42053d79038a8
TyBetter <TyBetter@users.noreply.github.com>
2021-03-24 14:40:20 +0800

    修改首页banner，打包发布1.0.0

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="container">
-		<u-swiper :list="list"></u-swiper>
+		<img src="./../../static/banner.jpg" alt="" srcset="" class="banner">
 		<ul class="list">
 			<li class="listItem" 
 				v-for="(item, index) in itemList" 
@@ -28,20 +28,6 @@
 		},
 		data() {
 			return {
-				list: [
-					{
-						image: 'https://cdn.uviewui.com/uview/swiper/1.jpg',
-						title: '昨夜星辰昨夜风,画楼西畔桂堂东'
-					},
-					{
-						image: 'https://cdn.uviewui.com/uview/swiper/2.jpg',
-						title: '身无彩凤双飞翼,心有灵犀一点通'
-					},
-					{
-						image: 'https://cdn.uviewui.com/uview/swiper/3.jpg',
-						title: '谁念西风独自凉,萧萧黄叶闭疏窗,沉思往事立残阳'
-					}
-				],
 				itemList:[]
 			}
 		},
@@ -65,6 +51,10 @@
 </script>
 
 <style scoped>
+	.banner {
+		height: 200rpx;
+		width: 100%;
+	}
 	.wrap {
 		padding: 40rpx;
 	}
